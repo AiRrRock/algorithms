82f2950352d5cbc780849dea25f92e2e65d343d6
Airt <alex.borichev@gmail.com>
2021-12-25 03:40:18 +0300

    Completed HW7

---
+++ Homework7/src/main/java/ru/gb/algorithms/MagicButtonApplication.java
@@ -0,0 +1,11 @@
+package ru.gb.algorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicButtonApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MagicButtonApplication.class, args);
+	}
+}
